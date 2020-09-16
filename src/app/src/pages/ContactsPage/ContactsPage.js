17fa5775e7e8e0e60dbdd4142a896209c65fe6fb
Vadim Prokopchuk <vprokopchuk@scnsoft.com>
2020-09-16 11:08:44 +0300

    contacts page example

--- src/app/src/pages/ContactsPage/ContactsPage.js
+++ src/app/src/pages/ContactsPage/ContactsPage.js
@@ -1,5 +1,44 @@
 import React from "react";
+import Card from "@material-ui/core/Card";
+import Grid from "@material-ui/core/Grid";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import CardActions from "@material-ui/core/CardActions";
+import Button from "@material-ui/core/Button";
 
 export default () => (
-    <h3>Contacts page</h3>
+    <Grid container spacing={1}>
+        {[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30].map(x => {
+            return (
+                <Grid item xs={2} key={x}>
+                    <Card>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                alt="Contemplative Reptile"
+                                height="140"
+                                image="/static/images/cards/contemplative-reptile.jpg"
+                                title="Contemplative Reptile"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                    Lizard {x}
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                                Message
+                            </Button>
+                            <Button size="small" color="primary">
+                                Open Profile
+                            </Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+            );
+        })}
+    </Grid>
 );
