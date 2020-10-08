37d38fdb9bfd5287c84ecbbfa93b2360213a84e1
Vadim Prokopchuk <vadim.prokopchuk@ocsico.com>
2020-10-08 20:32:48 +0300

    Added redux

--- src/app/src/App.js
+++ src/app/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import { BrowserRouter } from "react-router-dom";
+import { createStore, compose, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reduxThunk from 'redux-thunk';
+
 import Layout from "./components/Layout/Layout";
+import rootReducer from "./store/reducers/rootReducer";
+
+const store = createStore(
+    rootReducer,
+    compose(applyMiddleware(reduxThunk))
+);
 
 export default () => (
-    <BrowserRouter>
-        <Layout />
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <Layout />
+        </BrowserRouter>
+    </Provider>
 );
