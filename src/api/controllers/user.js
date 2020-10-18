023486b099299bb462a69039700201e4a1d492f6
Vadim Prokopchuk <vadim.prokopchuk@ocsico.com>
2020-10-18 20:00:17 +0300

    Refactor _id

--- src/api/controllers/user.js
+++ src/api/controllers/user.js
@@ -1,7 +1,10 @@
 const getUser = (req, res) => {
     const { user } = req.context;
-    const { __v, password, ...userData } = user._doc;
-    res.send(userData);
+    const { __v, password, _id, ...userData } = user._doc;
+    res.send({
+        id: _id,
+        ...userData
+    });
 }
 
 module.exports = {
