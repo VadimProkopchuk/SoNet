cdfa7b626b3d84272205da5eae5a605577a0b508
Vadim Prokopchuk <vadim.prokopchuk@ocsico.com>
2020-10-24 00:20:37 +0300

    Added Scroll Panel

--- src/app/src/components/Layout/AppMain/AppMain.js
+++ src/app/src/components/Layout/AppMain/AppMain.js
@@ -1,10 +1,12 @@
 import React from "react";
 import classes from "./AppMain.module.css";
+import "./AppMain.css";
 import {Redirect, Route, Switch} from "react-router-dom";
 import {Contacts, Messages, NotFound, Profile, Settings, SignIn, SignUp} from "../../../pages";
 import SideMenu from "../SideMenu";
 import Logout from "../../Logout/Logout";
 import Tests from "../../../pages/Tests/Tests";
+import {ScrollPanel} from "primereact/scrollpanel";
 
 const AppMain = ({isAuthenticated}) => {
     if (!isAuthenticated) {
@@ -24,16 +26,18 @@ const AppMain = ({isAuthenticated}) => {
             </div>
 
             <div className="p-offset-2 p-col-10">
-                <Switch>
-                    <Route exact path="/" component={Profile} />
-                    <Route path="/messages" component={Messages} />
-                    <Route path="/contacts" component={Contacts} />
-                    <Route path="/settings" component={Settings} />
-                    <Route path="/sign-in" component={SignIn} />
-                    <Route path="/logout" component={Logout} />
-                    <Route path="/tests" component={Tests} />
-                    <Route path="*" component={NotFound} />
-                </Switch>
+                <ScrollPanel className={classes.CustomBar}>
+                    <Switch>
+                        <Route exact path="/" component={Profile}/>
+                        <Route path="/messages" component={Messages}/>
+                        <Route path="/contacts" component={Contacts}/>
+                        <Route path="/settings" component={Settings}/>
+                        <Route path="/sign-in" component={SignIn}/>
+                        <Route path="/logout" component={Logout}/>
+                        <Route path="/tests" component={Tests}/>
+                        <Route path="*" component={NotFound}/>
+                    </Switch>
+                </ScrollPanel>
             </div>
         </React.Fragment>
     );
